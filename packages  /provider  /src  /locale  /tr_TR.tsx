8adb7915424356ea9084b065f222cc7a85e74a62
Laraxsry <33424360+Laraxsry@users.noreply.github.com>
2020-12-22 06:40:27 +0300

    Update and rename packages /provider /src /locale /tr_TR.tsx to packages  /provider  /src  /locale  /tr_TR.tsx

---
+++ packages  /provider  /src  /locale  /tr_TR.tsx
@@ -0,0 +1,56 @@
+export default {  
+  moneySymbol: '$',  
+  form: {  
+    lightFilter: {  
+      more: 'Daha Fazla',  
+      clear: 'Temizle',  
+      confirm: 'Onayla',  
+      itemUnit: 'Öğeler',  
+    },
+  },
+  tableForm: {  
+    search: 'Ara',  
+    reset: 'Sıfırla',  
+    submit: 'Gönder',  
+    collapsed: 'Daralt',  
+    expand: 'Genişlet',  
+    inputPlaceholder: 'Lütfen Giriniz',  
+    selectPlaceholder: 'Lütfen Seçiniz',  
+  },
+  alert: {  
+    clear: 'Temizle',  
+    selected: 'Seçildi',  
+    item: 'Öğe',  
+  },
+  pagination: {  
+    total: {  
+      range: '',  
+      total: 'Toplam',  
+      item: 'Öpe',
+    },
+  },
+  tableToolBar: {  
+    leftPin: 'Sola Tuttur',  
+    rightPin Pin to: 'Sağa Tuttur',  
+    noPin pinned: 'Tutturulmadı',  
+    leftFixedTitle: 'Sola Sabitle',
+    rightFixedTitle: 'Sağa Sabitle',
+    noFixedTitle: 'Sabitlenmedi',
+    reset: 'Sıfırla',
+    columnDisplay: 'Sütun Ekranı',  
+    columnSetting: 'Ayarlar',  
+    fullScreen: 'Tam Ekran',  
+    exitFullScreen: 'Tam Ekranı Kapat',  
+    reload: 'Yenile',
+    density: 'Kalınlık',  
+    densityDefault: 'Varsayılan',  
+    densityLarger: 'Büyük',  
+    densityMiddle: 'Orta',  
+    densitySmall: 'Küçük',  
+  },
+  stepsForm: {  
+    next: 'Sıradaki',  
+    prev: 'Önceki',  
+    submit: 'Tamamla',  
+  },
+};
